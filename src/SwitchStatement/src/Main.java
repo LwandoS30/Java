7fda7a372c6a5b23d89a1420442d9d84d679dc2d
Lwando <Lwando.Sodinga@fnb.co.za>
2026-06-15 11:35:43 +0200

    Section 5 Practice tasks

--- src/SwitchStatement/src/Main.java
+++ src/SwitchStatement/src/Main.java
@@ -1,15 +1,21 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main {
+    private static String month;
+
+    public static void main(String[] args) {
+        getQuarter("January");
+        System.out.println();
+    }
+
+    public  static  String getQuarter(String month){
+        Main.month = month;
+
+        return switch (month) {
+            case "January", "February", "March" -> "1st";
+            case "April", "May", "June" -> "2nd";
+            case "July", "August", "September" -> "3rd";
+            case "October", "November", "December" -> "4th";
+            default -> "Bad request month";
+        };
     }
 }
